efc3e5829d6ad1fbfdb65a8f16b1fdebc2fe9d07
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-31 11:32:19 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,44 @@
 #!/usr/bin/python3
 """Flask App that integrates with AirBnB static"""
-from flask import Flask
+from flask import Flask,make_response,jsonify
 from flask import sql_alchemy
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
+from os import getenv
+from flasgger import Swagger
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+# flask server environmental setup
+host = getenv('HBNB_API_HOST', '0.0.0.0')
+port = getenv('HBNB_API_PORT', 5000)
 
-def downtear(self):
-   """this method calls .close()"""
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.config['SWAGGER'] = {
+    "swagger_version": "2.0",
+    "title": "Flasgger",
+    "headers": [
+        ('Access-Control-Allow-Origin', '*'),
+        ('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE, OPTIONS"),
+        ('Access-Control-Allow-Credentials', "true"),
+    ],
+    "specs": [
+        {
+            "version": "1.0",
+            "title": "HBNB API",
+            "endpoint": 'v1_views',
+            "description": 'HBNB REST API',
+            "route": '/v1/views',
+        }
+    ]
+}
+swagger = Swagger(app)
+
+@app.teardown_appcontext
+def tear(self):
+    """this method calls .close()"""
     storage.close()
 
 @app.errorhandler(404)
@@ -19,10 +47,5 @@ def err(error):
     return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST')
-    port = getenv('HBNB_API_PORT')
-    if not port:
-        port = "5000"
-    if not host:
-        host = "0.0.0.0"
+    """Flask app"""
     app.run(host=host, port=port, threaded=True)
