4f543bb206c33745e464c11d0a5d3dbc00612c6f
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-31 12:33:23 +0300

    Update states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,7 +7,7 @@ from models.state import State
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
-def get_states():
+def get_state():
     """ Retrieves all State objects """
     states = storage.all(State)
     return jsonify([obj.to_dict() for obj in states.values()])
@@ -22,7 +22,7 @@ def with_state_id(state_id):
 
 @app_views.route('/states/<state_id>', methods=['DELETE'],
                  strict_slashes=False)
-def del_state(state_id):
+def delete_state(state_id):
     """ Deletes a State object """
     state = storage.get("State", state_id)
     if state is None:
@@ -32,7 +32,7 @@ def del_state(state_id):
     return make_response(jsonify({}), 200)
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
-def post_state():
+def post_a_state():
     """ Creates a state object """
     new_state = request.get_json()
     if not new_state:
@@ -46,7 +46,7 @@ def post_state():
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
-def put_state(state_id):
+def put_a_state(state_id):
     """ Updates a state"""
     state = storage.get("State", state_id)
     if not state:
@@ -56,9 +56,9 @@ def put_state(state_id):
     if not body_request:
         abort(400, "Not a JSON")
 
-    for k, v in body_request.items():
-        if k != 'id' and k != 'created_at' and k != 'updated_at':
-            setattr(state, k, v)
+    for key, value in body_request.items():
+        if key != 'id' and key != 'created_at' and key != 'updated_at':
+            setattr(state, key, value)
 
     storage.save()
     return make_response(jsonify(state.to_dict()), 200)
