6c78da182a098fa42ef565d20c12a9dda3380ff5
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-07-31 12:18:14 +0300

    Update places_reviews.py

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -25,28 +25,27 @@ def r_review_id(review_id):
         abort(404)
     return jsonify(review.to_dict())
 
-@app_views.route('/places/<place_id>/reviews', methods=['POST'],
-                 strict_slashes=False)
+
+@app_views.route('/places/<place_id>/reviews', methods=['POST'], strict_slashes=False)
 def post_review(place_id):
-    """ Creates a Review object """
+    """Creates a Review object"""
     place = storage.get("Place", place_id)
     if not place:
         abort(404)
-    new_review = request.get_json()
-    if not new_review:
-        abort(400, "Not a JSON")
-    if "user_id" not in new_review:
-        abort(400, "Missing user_id")
-    user_id = new_review['user_id']
+
+    new_review_data = request.get_json()
+    if not new_review_data or not all(key in new_review_data for key in ["user_id", "text"]):
+        abort(400, "Invalid data format. 'user_id' and 'text' are required.")
+
+    user_id = new_review_data['user_id']
     if not storage.get("User", user_id):
-        abort(404)
-    if "text" not in new_review:
-        abort(400, "Missing text")
-    review = Review(**new_review)
+        abort(404, "User not found.")
+
+    review = Review(**new_review_data)
     setattr(review, 'place_id', place_id)
     storage.new(review)
     storage.save()
-    return make_response(jsonify(review.to_dict()), 201)
+    return jsonify(review.to_dict()), 201
 
 
 @app_views.route('/reviews/<review_id>', methods=['PUT'],
@@ -72,10 +71,10 @@ def put_review(review_id):
 @app_views.route('/reviews/<review_id>', methods=['DELETE'],
                  strict_slashes=False)
 def del_review(review_id):
-    """ Deletes a Review """
+    """Deletes a Review"""
     review = storage.get("Review", review_id)
     if not review:
         abort(404)
-    review.delete()
+    storage.delete(review)
     storage.save()
-    return make_response(jsonify({}), 200)
+    return jsonify({}), 200
