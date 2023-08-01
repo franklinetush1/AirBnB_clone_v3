be7f2971598edc62fc02044bbf1acd85fef1288d
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-08-01 11:09:07 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,18 +17,18 @@ port = getenv('HBNB_API_PORT', 5000)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.config['SWAGGER'] = {
     "swagger_version": "2.0",
-    "title": "Flasgger",
+    "title": "AirBnb_V3",
     "headers": [
         ('Access-Control-Allow-Origin', '*'),
-        ('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE, OPTIONS"),
+        ('Access-Control-Allow-Methods', "GET, DELETE, OPTIONS, POST, PUT"),
         ('Access-Control-Allow-Credentials', "true"),
     ],
     "specs": [
         {
             "version": "1.0",
-            "title": "HBNB API",
+            "title": "AIRBNB_V3 API",
             "endpoint": 'v1_views',
-            "description": 'HBNB REST API',
+            "description": 'AIRBNB_V3 REST API',
             "route": '/v1/views',
         }
     ]
