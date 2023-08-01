05ce6ecc1e31c413014f42c4a6e625ff101495fd
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-08-01 12:44:46 +0300

    Update states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Configuration for State objects"""
 
+
 from api.v1.views import app_views
 from flask import jsonify, abort, make_response, request
 from models import storage
